b3e83f6474b6cead89537b3e1ab276bb4b770715
Dev Parapalli <30196311+DevParapalli@users.noreply.github.com>
2021-06-02 18:57:02 +0530

    better fib, more granular cbse-like grading

--- 16_grading.py
+++ 16_grading.py
@@ -1,38 +1,69 @@
 "Write a Program to enter the 5 subjects numbers and print the grades A/B/C/D/E"
 
-__max_marks__ = 80
+max_marks = 80
+
 
 def result(mark):
-    __metric = mark / (__max_marks__)
-    if __metric <= 0.4125: return "F"
-    if __metric <= 0.625: return "D"
-    if __metric <= 0.75: return "C"
-    if __metric <= 0.875: return "B"
-    if __metric <= 1.0: return "A"
+    metric = mark / max_marks
+    # Validation
+    if metric > 1:
+        raise ValueError(f'Value {mark} is greater than maximum ({max_marks})')
+    if metric < 0:
+        raise ValueError(f'Marks cannot be negative.')
+    # Actual Grading Logic
+    if metric <= .20:
+        return "E2"
+    if metric <= .32:
+        return "E1"
+    if metric <= .40:
+        return "D"
+    if metric <= .50:
+        return "C2"
+    if metric <= .60:
+        return "C1"
+    if metric <= .70:
+        return "B2"
+    if metric <= .80:
+        return "B1"
+    if metric <= .90:
+        return "A2"
+    if metric <= 1.0:
+        return "A1"
+
 
 def main():
     print("let maxMarks = 80;")
-    marks = {
-        "subject_a": int(input("Enter Marks for Subject A: ")),
-        "subject_b": int(input("Enter Marks for Subject B: ")),
-        "subject_c": int(input("Enter Marks for Subject C: ")),
-        "subject_d": int(input("Enter Marks for Subject D: ")),
-        "subject_e": int(input("Enter Marks for Subject E: ")),
-    }
+    try:
+        marks = {
+            "subject_a": int(input("Enter Marks for Physics> ")),
+            "subject_b": int(input("Enter Marks for Chemistry> ")),
+            "subject_c": int(input("Enter Marks for Maths/Bio> ")),
+            "subject_d": int(input("Enter Marks for English> ")),
+            "subject_e": int(input("Enter Marks for PE/CS> ")),
+        }
+    except ValueError:
+        print('Hmm, Is that a non-integral value? Marks are integers, you know.')
+        main()
+    for key in marks:
+        if marks[key] > max_marks:
+            raise ValueError(f'Value {marks[key]} is greater than maximum ({max_marks})')
+    # We 
     total = sum([marks[key] for key in marks])
-    average = total / 5
+    average = total / len(marks)
+    
+    # Formatted Strings to look good.
     print(f"""
-Grade for A: {result(marks["subject_a"])},
-Grade for B: {result(marks["subject_b"])},
-Grade for C: {result(marks["subject_c"])},
-Grade for D: {result(marks["subject_d"])},
-Grade for E: {result(marks["subject_e"])},
+Grade for Physics: {result(marks["subject_a"])},
+Grade for Chemistry: {result(marks["subject_b"])},
+Grade for Maths/Bio: {result(marks["subject_c"])},
+Grade for English: {result(marks["subject_d"])},
+Grade for PE/CS: {result(marks["subject_e"])},
 Average Grade: {result(average)}
     """)
 
+
 if __name__ == "__main__":
     main()
-
 
 
 __OUTPUT__ = """
--- 7_fibonacci.py
+++ 7_fibonacci.py
@@ -1,37 +1,27 @@
 "Write a Program to enter the number of terms and to print the Fibonacci Series."
 from math import sqrt
 
-def fibonacci(number):
-    if number == 0:
-        return 0
-    elif number == 1:
-        return 1
-    elif number < 0:
-        return 'ValueError: Value too small.'
-    else:
-        return fibonacci(number-1) + fibonacci(number-2)
-    
 
-def alternate_fib(number):
+def fibonacci(number:int):
     sqrt_5 = sqrt(5)
     phi = (1 + sqrt_5)/2
     psi = (1 - sqrt_5)/2
     return round((phi**number - psi**number)/sqrt_5)
 
+
 def main():
     import time
-    __no_of_terms = int(input("Number of Terms of Fibonacci: "))
-    #t1 = time.time()
-    #__list = [fibonacci(number) for number in range(__no_of_terms)]
-    #print(f"Fibonacci Numbers up to {__no_of_terms}th term is {__list} ")
-    t2 = time.time()
-    __list_alt = [alternate_fib(number) for number in range(__no_of_terms)]
-    print(f"Fibonacci Numbers up to {__no_of_terms}th term is {__list_alt} ")
-    #print("A {}".format(time.time() - t1))
-    print("B {}".format(time.time() - t2))
+    no_of_terms = int(input("Number of Terms of Fibonacci: "))
+    t1 = time.time()
+    list_of_fib_num = [fibonacci(number) for number in range(no_of_terms)]
+    print(
+        f"Fibonacci Numbers up to {no_of_terms}th term is {list_of_fib_num} ")
+    print("Time Taken: {}".format(time.time() - t1))
+
+
 if __name__ == "__main__":
     main()
-    
+
 __OUTPUT__ = """
 Fibonacci Numbers up to 13th term is [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
 """
